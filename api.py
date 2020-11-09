1fc255e7a40838627364ca8a6708091415c086c2
Wincioor11 <wincioor11@gmail.com>
2020-11-09 12:41:36 +0100

    port change

--- api.py
+++ api.py
@@ -64,4 +64,4 @@ def colorize(colorizer_service: ImageColorizerService):
 # setup flask dependency injection 
 FlaskInjector(app=app, modules=[configure])
 # run api
-app.run(port=os.environ.get('PORT', 33507)))
+app.run(port=os.environ.get('PORT', 33507))
