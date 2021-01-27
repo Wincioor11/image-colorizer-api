eca4fe205684b935fa1f75c3cd1d217010831960
Wincioor11 <wincioor11@gmail.com>
2021-01-27 17:15:11 +0100

    final

--- app.py
+++ app.py
@@ -7,35 +7,34 @@ from PIL import Image
 import io
 import base64
 import os
-# from dotenv import load_dotenv
-
+from dotenv import load_dotenv
 
 from resourcelayer.ImageColorizerService import ImageColorizerService
 from dependencies import configure
 from gdrive_download import download_file_from_google_drive
+# initialize GPU for tensorflow (comment it when using machine without NVIDIA GPU)
+import tensorflow as tf
+gpu = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpu[0], True)
 
-
-
-
-# initialize GPU for tensorflow (uncomment it only on local machine, not when deployed)
-# import tensorflow as tf
-# gpu = tf.config.experimental.list_physical_devices('GPU')
-# tf.config.experimental.set_memory_growth(gpu[0], True)
-
+# Initialize flask app
 app = flask.Flask(__name__)
+# Enable CORS
 CORS(app)
-# load_dotenv()
+# Load variables from .env file as environment variables
+load_dotenv()
+# Set app config from env vars
 app.config['DEBUG'] = os.environ.get('DEBUG')
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['ACCESS_KEY'] = os.environ.get('ACCESS_KEY')
 app.config['PORT'] = os.environ.get('PORT')
 
-#Download large model file from google drive
-MODEL_PATH = 'generator_model_256_v3.h5'
-FILE_ID = os.environ.get('FILE_ID')
-if not os.path.isfile(MODEL_PATH):
-    print('Model not found')
-    download_file_from_google_drive(FILE_ID,MODEL_PATH)
+# Download large model file from google drive if doesn't exist
+# MODEL_PATH = 'static/trained_models/places365/generator_model_256_v3.h5'
+# FILE_ID = os.environ.get('FILE_ID')
+# if not os.path.isfile(MODEL_PATH):
+#     print('Model not found')
+#     download_file_from_google_drive(FILE_ID,MODEL_PATH)
+#     print('Model loaded from google drive')
 
 def verify_access(key: bytes):
     if key == app.config.get('ACCESS_KEY'):
@@ -43,15 +42,11 @@ def verify_access(key: bytes):
     else:
         return False
 
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>Api is working fine :)</h1>"
-
 @inject
 @app.route('/colorize', methods=['POST'])
 def colorize(colorizer_service: ImageColorizerService):
-    # check is ACCESS_KEY is valid
-    key = flask.request.headers.get('ACCESS_KEY')#,as_bytes=True)
+    # check access
+    key = flask.request.headers.get('Authorization')#,as_bytes=True)
     if not verify_access(key=key):
         return flask.Response(status=401)
     else:
@@ -62,23 +57,17 @@ def colorize(colorizer_service: ImageColorizerService):
             # colorizer = ImageColorizerService()
             img_colored = colorizer_service.colorize(img)
             image = Image.fromarray(img_colored.astype('uint8'))
-            # img1 = io.FileIO()
-            # image.save(img1, "PNG")
-            # img1.seek(0)
 
             rawBytes = io.BytesIO()
             image.save(rawBytes, "JPEG")
-            # rawBytes.seek(0)  # return to the start of the file
 
             b64 = base64.b64encode(rawBytes.getvalue())
-            # b64 = base64.encodebytes(rawBytes)
             status_code = 200
         except Exception as ex:
+            print(ex)
             status_code = 500
             b64 = None
-            # rawBytes = None
 
-        # return flask.send_file(b64, mimetype='image/jpeg')
         return flask.Response(b64, status=status_code)
 
 # setup flask dependency injection 
@@ -86,5 +75,4 @@ FlaskInjector(app=app, modules=[configure])
 
 if __name__ == "__main__":
     # run api
-    # app.run(port=os.environ.get('PORT', 33507))
     app.run(port=app.config.get('PORT'))
--- resourcelayer/ImageColorizerService.py
+++ resourcelayer/ImageColorizerService.py
@@ -1,37 +1,41 @@
 from keras.models import load_model
-from keras.preprocessing.image import img_to_array
-from keras.preprocessing.image import load_img
 import numpy as np
 import cv2
 
 class ImageColorizerService():
-    def __init__(self, generator_path: str ='generator_model_256_v3.h5'):
+    def __init__(self, generator_path: str ='static/trained_models/places365/generator_model_256_v3.h5'):
         self.generator = load_model(generator_path)
         self.generator.trainable = False
     
     def colorize(self, img):
         img = np.asarray(img)
-        org_size = img.shape[1], img.shape[0] # numpy's shape is transposed somehow :)
+        # swap shape, because numpy's shape is transposed
+        org_size = img.shape[1], img.shape[0] 
         resize_flag = False
+        img_gray_org, _ = self.rgb2lab(img)
         if (org_size != (256,256)):
             resize_flag = True
             img = self.resize_image(img, size=(256,256))
         img_gray, _ = self.rgb2lab(img)
         img_generated = self.generator.predict(np.expand_dims(img_gray,axis=0))
+        # transform to rgb
         img_rgb = self.lab2rgb(img_l=img_gray,img_ab=img_generated[0], size=(256,256,3))
         if (resize_flag):
             img_rgb = self.resize_image(img_rgb, size=org_size)
+        # replace L layer to orginal one -> avoid quality loss
+        img_gray, img_ab = self.rgb2lab(img_rgb)
+        img_rgb = self.lab2rgb(img_l=img_gray_org,img_ab=img_ab, size=img_rgb.shape)
         return img_rgb
 
     def resize_image(self, img: np.ndarray, size=(256,256)):
-        img = cv2.resize(img, size, interpolation=cv2.INTER_AREA)
+        img = cv2.resize(img, size, interpolation=cv2.INTER_LINEAR)
         return img
     
     def rgb2lab(self, img: np.ndarray, normalize=True):
         # transform to lab
         img_lab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
-        img_l = np.zeros((img_lab.shape[0],img_lab.shape[1],1))#, dtype=np.uint8)
-        img_ab  = np.zeros((img_lab.shape[0],img_lab.shape[1],2))#, dtype=np.uint8)
+        img_l = np.zeros((img_lab.shape[0],img_lab.shape[1],1))
+        img_ab  = np.zeros((img_lab.shape[0],img_lab.shape[1],2))
         if normalize:
             # normalize
             img_l[:, :, 0] = (img_lab[:, :, 0]-127.5 )/127.5
@@ -43,9 +47,9 @@ class ImageColorizerService():
         return img_l, img_ab # l, ab
     
     def lab2rgb(self, img_l: np.ndarray, img_ab: np.ndarray, size=(256,256,3)):
-        # Initializing img arrat with zeros (or any random number)
+        # Initializing img array with zeros
         img = np.zeros(size)
-        # Scaling from [-1,-1] to [0,255] and deleting fake dimension from grayscale :)
+        # Scaling from [-1,-1] to [0,255]
         img[:, :, 0] = (img_l[:, :, 0] + 1.0) / 2.0 *255.
         img[:, :, 1:] = (img_ab + 1.0) / 2.0 *255.
         img = img.astype('uint8')
